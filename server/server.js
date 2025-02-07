0e7a82f2f4e118d324171f6d74f57fa4e824ee38
Dey <Day4y3@proton.me>
2025-02-07 12:42:08 +0100

    added FRONTEND_URL to .env

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 let corsOptions = {
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
     methods: "GET,PUT,POST,DELETE",
     credentials: true
 };
